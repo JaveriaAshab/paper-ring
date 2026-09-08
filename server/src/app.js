ad2bf5c432d4dbfc243878caa85033ad6d64ecc0
Javeria Ashab <162408756+JaveriaAshab@users.noreply.github.com>
2026-09-08 13:37:26 +0500

    Improve error logging for API errors

--- server/src/app.js
+++ server/src/app.js
@@ -59,7 +59,8 @@ app.use((_req, res) => {
 });
 
 app.use((err, _req, res, _next) => {
-  console.error(err);
+  console.error("Paper Ring API error:", err);
+
   res.status(err.status || 500).json({
     message: err.message || "Something went wrong."
   });
